ef194c327f797c644681ff183ae7675903806500
steefanyalvees <stefanyanjoos@hotmail.com>
2025-01-02 07:55:42 +0000

    python

--- sabor_express/exercise3.py
+++ sabor_express/exercise3.py
@@ -1,10 +1,13 @@
 #age = 22
 #nome = "Stefany"
 
-nome = (input("type a name"))
-print(nome)
- 
-if nome == "Stefany":  
-    print(" beautil name")
+usuario_email = "stefany@"
+usuario_name = "stefany"
+
+usuario = input(" ola, digite o email")
+name = input("digite usuario nome")
+
+if usuario == usuario_email and name == usuario_name:
+    print("bem vindo")
 else:
-    print(" not nice")
+    print("errado, tente dnv")
