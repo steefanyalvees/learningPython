0cb627cf528a5b8b9eac0c65a41b2b1d573db0ce
steefanyalvees <stefanyanjoos@hotmail.com>
2025-01-07 05:31:15 +0000

    exercises

---
+++ exercises_modulo_1/exercise1.py
@@ -0,0 +1,25 @@
+#1 - Crie uma lista para cada informação a seguir:
+
+#Lista de números de 1 a 10;
+list_number = [1,2,3,4,5,6,7,8,9,10]
+#Lista com quatro nomes;
+lista_name =['Ste', 'Le']
+#Lista com o ano que você nasceu e o ano atual.
+lista_year = [1997,1990]
+print(list_number, lista_name, lista_year)
+
+#2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+numeros = [1,2,3,4,5]
+for numero in numeros:
+    print(f'{numeros}')
+
+#3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+numero_impar = [1, 3, 5, 7, 9]
+for numero in numero_impar:
+    print (numero_impar)
+   # numero_impar.append
+
+#4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
+numero_impar = [1,3,5,7,9]
+for numero in numero_impar:
+   print (numero_impar)
